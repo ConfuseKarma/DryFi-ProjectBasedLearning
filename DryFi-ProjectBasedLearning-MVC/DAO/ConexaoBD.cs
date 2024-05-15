8888dc11fc7849531908d688a076a798c3ec1817
GUILHERME RODRIGUES DOS SANTOS <081220010@faculdade.cefsa.edu.br>
2024-05-15 20:47:57 -0300

    Mudanças visuais

--- DryFi-ProjectBasedLearning-MVC/DAO/ConexaoBD.cs
+++ DryFi-ProjectBasedLearning-MVC/DAO/ConexaoBD.cs
@@ -16,13 +16,13 @@ namespace DryFi_ProjectBasedLearning_MVC.DAO
         public static SqlConnection GetConexao()
         {
             //String LAB
-            //string strCon = "Data Source=LOCALHOST; Database=AULADB; user id=sa; password=123456";
+            string strCon = "Data Source=LOCALHOST; Database=AULADB; user id=sa; password=123456";
             
             //String EDUARDO TOLEDO
             //string strCon = "Data Source=DESKTOP-R99OVQ8\\SQLEXPRESS; Database=linguagemProgramacao; integrated security=true";
 
             //String GUILHERME SANTOS
-            string strCon = "Data Source=GUILHERME\\SQLSERVER2022; Database=AulaDB; integrated security=true";
+            //string strCon = "Data Source=GUILHERME\\SQLSERVER2022; Database=AulaDB; integrated security=true";
 
             SqlConnection conexao = new SqlConnection(strCon);
             conexao.Open();
